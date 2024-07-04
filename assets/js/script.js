4e6d2024ae1276bcb2eec44fbc6e375790d1a960
chichuichen77 <77emilychen@gmail.com>
2024-07-05 06:51:21 +0800

    update pages and javascripts.

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,19 +79,17 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
   for (let i = 0; i < filterItems.length; i++) {
-
+    const categories = filterItems[i].dataset.category.toLowerCase().split(',').map(category => category.trim());
+    
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
+    } else if (categories.includes(selectedValue)) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
-
 }
 
 // add event in all filter button items for large screen
